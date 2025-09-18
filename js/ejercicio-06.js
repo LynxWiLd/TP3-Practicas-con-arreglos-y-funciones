eb770b8ea2a90405837e493013cadccf0de9b3f1
Vera_Facundo <facundovera647@gmail.com>
2025-09-17 23:23:26 -0300

    ejercicio06 terminado

--- js/ejercicio-06.js
+++ js/ejercicio-06.js
@@ -1,2 +1,12 @@
 /* Solicitar por pantalla al usuario ingresar el valor de los lados de un rectángulo, luego crear una función para calcular su perímetro y mostrarlo por pantalla.
  */
+function perimetro(base, altura) {
+  document.writeln(
+    `El perimetro de su rectangulo es de ${2 * (base + altura)}`
+  );
+}
+
+alert("CALCULEMOS EL PERIMETRO DE UN RECTANGULO");
+const base = parseInt(prompt("Ingrese la Base:"));
+const altura = parseInt(prompt("Ingrese la Altura:"));
+perimetro(base, altura);
