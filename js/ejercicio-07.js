20ada58e2be0533c035fff37d7f1d805012decb1
Vera_Facundo <facundovera647@gmail.com>
2025-09-17 23:09:20 -0300

    ejercicio7 terminado

--- js/ejercicio-07.js
+++ js/ejercicio-07.js
@@ -1,1 +1,12 @@
 /*Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10 del número elegido por el usuario. */
+function tabla(numero) {
+  document.writeln(`<h3>TABLA DE MULTIPLICAR DEL ${numero}</h3>`);
+  for (let i = 0; i <= 10; i++) {
+    document.writeln(`${numero} x ${i} = ${numero * i}<br>`);
+  }
+}
+
+const numero = parseInt(
+  prompt("Ingrese el numero para mostrar su tabla de multiplicar:")
+);
+tabla(numero);
